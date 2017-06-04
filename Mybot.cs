47879030599eff4cb6203b2463929b91660e89e0
leminhanh <mle098765321@gmail.com>
2017-06-04 22:56:44 +0700

    Add files via upload

---
+++ Mybot.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korwa_Bot_by_me
+{
+    class Mybot
+    {
+        DiscordClient discord;
+        CommandService commands;
+
+        public Mybot()
+        {
+            discord = new DiscordClient(x =>
+          {
+              x.LogLevel = LogSeverity.Info;
+              x.LogHandler = Log;
+          });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            commands = discord.GetService<CommandService>();
+
+            RegisterPurgeCommand();
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzIwMjQwMDExNDczNTg0MTMw.DBM_cg.UWkhf8DpCLetJqHn2CwW6Y24G5g", TokenType.Bot);
+            });
+        }
+
+        private void RegisterPurgeCommand()
+        {
+            commands.CreateCommand("purge")
+                .Do(async (e) =>
+                {
+                    Message[] messagesToDelete;
+                    messagesToDelete = await e.Channel.DownloadMessages(100);
+
+                    await e.Channel.DeleteMessages(messagesToDelete);
+                });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
